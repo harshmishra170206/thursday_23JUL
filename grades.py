d22d29002093bded81f90114d2ddbdca30b98f2d
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-07-23 10:51:31 +0530

    Added average functionality

--- grades.py
+++ grades.py
@@ -7,6 +7,11 @@ def calculate_grade(marks):
     return "C"
  else:
     return "F"
+
+def average(marks_list):
+ return sum(marks_list) / len(marks_list)
+
+ 
 if __name__ == "__main__":
     marks = 92
     print("Marks:", marks)
